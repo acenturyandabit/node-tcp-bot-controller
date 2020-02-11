d05c7dcdc8f3725260e00f0c439e9b2b996fc6f1
Steven Liu <steeven.liu2@gmail.com>
2020-02-11 15:51:33 +1100

    flip controls

--- index.js
+++ index.js
@@ -42,13 +42,13 @@ const requestHandler = (request, response) => {
   
       function sendCommands(x, y) {
           if (x < w / 3) {
-              ping("0f1o2f3o");
+            ping("0o1f2o3f");
           } else if (x > 2 * w / 3) {
-              ping("0o1f2o3f");
+            ping("0f1o2f3o");
           } else if (y < h / 3) {
-              ping("0f1o2o3f");
+            ping("0o1f2f3o");
           } else if (y > 2 * h / 3) {
-              ping("0o1f2f3o");
+            ping("0f1o2o3f");
           } else {
               ping("2f3f0f1f");
           }

3c655436ca8a5956c3fd01af21a7e8f19eeaac85
Steven Liu <steeven.liu2@gmail.com>
2020-02-20 10:50:07 +1100

    disable pull to refresh

--- index.js
+++ index.js
@@ -16,7 +16,41 @@ const port = 8022
 
 const requestHandler = (request, response) => {
   console.log(request.url)
-  let stuff=`<canvas></canvas>
+  let stuff=`
+  <body class="body">
+  <div class="wrapper">
+  <canvas></canvas>
+  </div>
+</body>
+
+  <style>
+  .body,
+.wrapper {
+  /* Break the flow */
+  position: absolute;
+  top: 0px;
+
+  /* Give them all the available space */
+  width: 100%;
+  height: 100%;
+
+  /* Remove the margins if any */
+  margin: 0;
+
+  /* Allow them to scroll down the document */
+  overflow-y: hidden;
+}
+
+.body {
+  /* Sending body at the bottom of the stack */
+  z-index: 1;
+}
+
+.wrapper {
+  /* Making the wrapper stack above the body */
+  z-index: 2;
+}
+  </style>
   <script>
       document.body.style.margin = 0;
       let c = document.querySelector("canvas");
